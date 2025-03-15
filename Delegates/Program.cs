7340a130cda90342b27277c177179de4a936dd6b
Rafet <rafetr@risk.az>
2025-03-15 14:20:48 +0400

    exercise

---
+++ Delegates/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+        }
+        
+        static string Capitalize(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return word;
+
+            if (char.IsLetter(word[0]))
+            {
+                if (word.Length == 1)
+                    return word[0].ToString().ToUpper();
+
+                return word[0].ToString().ToUpper() + word.Substring(1);
+            }
+
+            return word;
+        }
+
+        static void IsEven(int number)
+        {
+            Console.WriteLine(number % 2 == 0);
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static List<T> MyWhere<T>(this List<T> source, Func<T, bool> predicate)
+        {
+            var list = new List<T>();
+
+            foreach (var item in source)
+            {
+                if (predicate.Invoke(item))
+                    list.Add(item);
+            }
+
+            return list;
+        }
+
+        public static T MyFind<T>(this List<T> source, Predicate<T> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate.Invoke(item))
+                {
+                    return item;
+                }
+            }
+
+            return default;
+        }
+
+        public static int MyIndexOf<T>(this List<T> source, Predicate<T> predicate)
+        {
+            for (int i = 0; i < source.Count; i++)
+            {
+                if (predicate.Invoke(source[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- NetflixPractise/Program.cs
+++ NetflixPractise/Program.cs
@@ -1,51 +1,68 @@
 ﻿using NetflixPractise.Models;
+using System.Net.Security;
 
 namespace NetflixPractise
 {
     internal class Program
     {
+        public delegate void Command();
+
         static void Main(string[] args)
         {
             Storage storage = new Storage();
+            var dict = new Dictionary<string, Command>
+            {
+                { "print genre", storage.PrintGenres },
+                { "add genre", storage.AddGenre }
+            };
 
             while (true)
             {
                 Console.WriteLine("Enter command:");
                 string command = Console.ReadLine();
 
-                switch (command)
+                if (dict.TryGetValue(command, out Command action))
                 {
-                    case "print genre":
-                        storage.PrintGenres();
-                        break;
-                    case "add genre":
-                        storage.AddGenre();
-                        break;
-                    case "remove genre":
-                        storage.RemoveGenre();
-                        break;
-                    case "print movie":
-                        storage.PrintMovie();
-                        break;
-                    case "add movie":
-                        storage.AddMovie();
-                        break;
-                    case "remove movie":
-                        storage.RemoveMovie();
-                        break;
-                    case "Watch movie":
-                        storage.Watch();
-                        break;
-                    case "Search movie":
-                        storage.Search();
-                        break;
-                    case "Most view":
-                        storage.MostViewedMovie();
-                        break;
-                    default:
-                        Console.WriteLine("Command not right");
-                        break;
+                    action.Invoke();
                 }
+                else
+                {
+                    Console.WriteLine("Command not right");
+                }
+
+                //switch (command)
+                //{
+                //    case "print genre":
+                //        storage.PrintGenres();
+                //        break;
+                //    case "add genre":
+                //        storage.AddGenre();
+                //        break;
+                //    case "remove genre":
+                //        storage.RemoveGenre();
+                //        break;
+                //    case "print movie":
+                //        storage.PrintMovie();
+                //        break;
+                //    case "add movie":
+                //        storage.AddMovie();
+                //        break;
+                //    case "remove movie":
+                //        storage.RemoveMovie();
+                //        break;
+                //    case "Watch movie":
+                //        storage.Watch();
+                //        break;
+                //    case "Search movie":
+                //        storage.Search();
+                //        break;
+                //    case "Most view":
+                //        storage.MostViewedMovie();
+                //        break;
+                //    default:
+                //        Console.WriteLine("Command not right");
+                //        break;
+                //}
             }
         }
     }
